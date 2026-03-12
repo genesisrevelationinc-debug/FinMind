411d650051e0e4bfdc8b29068290c2cafe26f98a
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-12 15:47:44 -0400

    fix: apply solution for issue #130

--- finmind/jobs/__init__.py
+++ finmind/jobs/__init__.py
@@ -5,4 +5,4 @@ from finmind.jobs.retry import RetryPolicy, ExponentialBackoff
 from finmind.jobs.monitor import JobMonitor, JobStatus
 from finmind.jobs.decorators import resilient_job
 
-__all__ = ['JobExecutor', 'RetryPolicy', 'ExponentialBackoff', 'JobMonitor', 'JobStatus', 'resilient_job']
+__all__ = ["JobExecutor", "RetryPolicy", "ExponentialBackoff", "JobMonitor", "JobStatus", "resilient_job"]
