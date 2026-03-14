079722202b1d0345b602a2c34116c8edec1bca88
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-14 19:44:38 -0400

    fix: apply solution for issue #130

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from .config import Config
-from .extensions import init_extensions, db, jwt, scheduler
+from .extensions import db, jwt, scheduler
 from .routes import register_routes
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object('backend.app.config.Config')
     init_extensions(app)
     register_routes(app)
     return app
