06a9d06d5156c0c5cc322bb63dc5a70871d2f34b
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-14 17:31:58 -0400

    fix: apply solution for issue #121

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,15 +3,11 @@ from .config import Config
 from .extensions import db
 from .routes import api_bp
 
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    app.config.from_object(Config)
 
     db.init_app(app)
-
     app.register_blueprint(api_bp)
-
-    with app.app_context():
-        db.create_all()
 
     return app
