80830458ca4042d60cebf59454361391355466cb
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-14 22:27:19 -0400

    fix: apply solution for issue #121

--- backend/app/models.py
+++ backend/app/models.py
@@ -24,6 +24,7 @@ class Bill(db.Model):
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
