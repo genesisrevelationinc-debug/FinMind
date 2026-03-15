fe8c00bb6b4ec44abd9d0908c1bb37dfbc9410f6
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-14 20:45:43 -0400

    fix: apply solution for issue #132

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,13 @@
 from flask import Flask
-from flask_jwt_extended import JWTManager
 from .config import Config
 from .extensions import db
 from .routes import api_bp
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
-    JWTManager(app)
 
     app.register_blueprint(api_bp)
 
