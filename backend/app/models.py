82fec8081b45e94bf32236af1a003782ee638188
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-18 03:14:57 -0400

    fix: apply solution for issue #121

--- backend/app/models.py
+++ backend/app/models.py
@@ -24,8 +24,6 @@ class Bill(db.Model):
 
 class User(db.Model):
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
