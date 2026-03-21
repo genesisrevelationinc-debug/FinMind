4aca1fe3e21be6b2ce304ef33083eaef95ca1eae
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-21 11:51:21 -0400

    fix: apply solution for issue #133

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,10 +1,5 @@
 from flask import Blueprint
-from .auth import auth_bp
-from .savings import savings_bp
-
-api_bp = Blueprint('api', __name__)
-
-api_bp.register_blueprint(auth_bp, url_prefix='/auth')
-api_bp.register_blueprint(savings_bp, url_prefix='/savings')
-
-from . import auth, savings
+from . import auth
+from . import savings_goals
+api = Blueprint('api', __name__, url_prefix='/api')
+api.register_blueprint(savings_goals.bp)
