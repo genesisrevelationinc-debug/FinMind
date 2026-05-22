da20bb16773445dbe7f4f48f6eaff62658831a18
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-22 10:00:26 -0400

    fix: apply solution for issue #75

---
+++ connector.py
@@ -0,0 +1,10 @@
+class BankConnector:
+    def __init__(self):
+        self.name = "Base"
+        self.type = "bank_connector"
+    
+    def connect(self, user_id):
+        pass
+    
+    def refresh_data(self, user_id):
+        pass
