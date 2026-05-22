99a680c7389cf02bd92a20111bcc5fd0ec09c1c7
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-22 10:00:28 -0400

    fix: apply solution for issue #75

--- connector.py
+++ connector.py
@@ -1,10 +1,16 @@
-class BankConnector:
+import abc
+
+
+class BankConnector(abc.ABC):
+    """Base class for all bank connectors"""
+    
     def __init__(self):
-        self.name = "Base"
         self.type = "bank_connector"
     
+    @abc.abstractmethod
     def connect(self, user_id):
         pass
     
+    @abc.abstractmethod
     def refresh_data(self, user_id):
         pass
